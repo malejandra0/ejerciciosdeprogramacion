3479d76eb75356288b10964616366c0fe3f0848f
maria alejandra <maria alejandra@vivoasuspc>
2024-08-12 19:47:34 -0500

    version 2

--- src/ejercicio/ejercicio4.java
+++ src/ejercicio/ejercicio4.java
@@ -7,29 +7,55 @@ import java.util.Scanner;
  *
  * @author maria alejandra
  */
-public class ejercicio4 { 
+public class ejercicio4 {
     /**
      * @param args the command line arguments
      */
-    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner entra= new Scanner(System.in);
-        System.out.println("ingrese los 10 numeros");
-        int []num= new int[10];
-        int a=0;
-        int suma=0;
-        int promedio=0;
-         while(a<10){
-            num[a]=entra.nextInt();
-            suma+=num[a];
-            a++;        
-            }
-         promedio=suma/10;
-         System.out.println("el promedio es de"+ promedio);
+        Scanner entra = new Scanner(System.in);
+        System.out.println("Ingrese los 10 números:");
+        int[] num = new int[10];
+        int[] nume = new int[10];
+        int suma = 0;
+        int promedio;
+
+        for (int a = 0; a < 10; a++) {
+            num[a] = entra.nextInt();
+            suma += num[a];
         }
-   
+
+        for (int z = 0; z < 10; z++) {
+            int temp = 0;
+            for (int y = 0; y < 10; y++) {
+                if (num[z] == num[y]) {
+                    temp++;
+                }
+            }
+            nume[z] = temp;
+        }
+
+        promedio = suma / 10;
+        String resultados = "";
+
+        for (int x = 0; x < 10; x++) {
+            boolean repetido = false;
+            for (int w = 0; w < x; w++) {  // Comparamos con los números anteriores, no todos
+                if (num[x] == num[w]) {
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido) {
+                resultados += "Número " + num[x] + " se repite " + nume[x] + " veces\n";
+            }
+        }
+
+        System.out.println("Los resultados son:\n" + resultados);
+        System.out.println("El promedio de los números es: " + promedio);
+    }
 }
+   
+
 
         
 
--- src/ejercicio/ejercicio5.java
+++ src/ejercicio/ejercicio5.java
@@ -13,7 +13,32 @@ public class ejercicio5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        int numAlumnos = 5;
+        int numNotas = 3;
+        double[][] notas = new double[numAlumnos][numNotas];
+        double[] promedios = new double[numAlumnos];
+        
+        // Ingreso de las notas
+        for (int i = 0; i < numAlumnos; i++) {
+            System.out.println("Ingrese las 3 notas del alumno " + (i + 1) + ":");
+            double suma = 0;
+            for (int j = 0; j < numNotas; j++) {
+                notas[i][j] = scanner.nextDouble();
+                suma += notas[i][j];
+            }
+            promedios[i] = suma / numNotas; // Calculamos el promedio y lo almacenamos en un array separado
+        }
+        
+        // Mostrar las notas y los promedios
+        System.out.println("\nNotas y promedios de los alumnos:");
+        for (int i = 0; i < numAlumnos; i++) {
+            System.out.print("Alumno " + (i + 1) + ": ");
+            for (int j = 0; j < numNotas; j++) {
+                System.out.print(notas[i][j] + " ");
+            }
+            System.out.println("| Promedio: " + promedios[i]);
+        }
     }
 }
 
